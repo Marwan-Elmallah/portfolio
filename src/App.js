c8f3030659e9e97d1bfd5dddb9344ab02882c51f
Marwan-Elmallah <engmarwan0110@gmail.com>
2023-02-18 15:27:05 +0200

    updates in proj

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import './App.css';
 import Contact from './components/Contact';
 import Footer from './components/footer';
@@ -16,9 +15,10 @@ function App() {
     let scrollPosition = window.scrollY
     if(scrollPosition >= 200){
         nav.style = "background: black; transition: 1s"
-      if(scrollPosition < 500){
+      if(scrollPosition < 600){
         mainImg.classList.add("animateImg")
-      }else if (scrollPosition > 500){
+        setTimeout(() => {mainImg.classList.remove("animateImg")},1000)
+      }else if (scrollPosition > 600){
         mainImg.classList.remove("animateImg")
         if(scrollPosition > 1900){
           contactImg.classList.add("animateImg")
